50218d5449c1e199697140f3de46c2349020e230
Giovanni De Cesare <giovanni.decesare@inaf.it>
2023-11-09 17:06:59 +0100

    build added

---
+++ build/CMakeFiles/cusp_gdml.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cusp_gdml.dir/cusp_gdml.cc.o"
+  "CMakeFiles/cusp_gdml.dir/cusp_gdml.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/ConfigFile.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/ConfigFile.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/DetectorHit.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/DetectorHit.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/SensitiveDetector.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/SensitiveDetector.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/UserActionInitialization.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/UserActionInitialization.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/UserEventAction.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/UserEventAction.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/UserRun.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/UserRun.cc.o.d"
+  "CMakeFiles/cusp_gdml.dir/src/UserRunAction.cc.o"
+  "CMakeFiles/cusp_gdml.dir/src/UserRunAction.cc.o.d"
+  "cusp_gdml"
+  "cusp_gdml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cusp_gdml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cusp_gdml.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cusp_gdml.
+# This may be replaced when dependencies are built.
