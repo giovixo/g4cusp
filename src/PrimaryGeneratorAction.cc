9f4cd12855e0892ed4c93f4aeb8217f9c1b3fd0a
Giovanni De Cesare <giovanni.decesare@inaf.it>
2025-01-15 16:32:51 +0100

    zero size beam model (for the test see the screenshot)

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -28,16 +28,23 @@ PrimaryGeneratorAction::~PrimaryGeneratorAction() {
     delete particleGun;
 }
 
-void PrimaryGeneratorAction::GenerateVertex(G4ThreeVector& vertex) {
-    vertex.setX(0.*cm);
-    vertex.setY(10.*cm);
-    vertex.setZ(0.*cm);
+void PrimaryGeneratorAction::GenerateVertex(G4double source_h, G4double source_r, G4double source_alpha, G4ThreeVector& vertex) {
+    G4double y = source_h + 4.*cm ; // TdDO: check the exact position of the detector surface (detector center = 10.5*cm?)
+    G4double z = source_r * std::cos(source_alpha); 
+    G4double x = source_r * std::sin(source_alpha);
+    vertex.setX(x);
+    vertex.setY(y);
+    vertex.setZ(z);
 }
 
-void PrimaryGeneratorAction::GenerateDirection(G4ThreeVector& direction) {
-    direction.setX(0.);
-    direction.setY(-1.);
-    direction.setZ(0.);
+void PrimaryGeneratorAction::GenerateDirection(G4double detector_h, G4double beam_size1, G4double beam_size2, G4ThreeVector vertex, G4ThreeVector& direction) {
+    // Define the center of the detector surface
+    G4ThreeVector detector_center;
+    detector_center.setX(0.);
+    detector_center.setY(detector_h);
+    detector_center.setZ(0.);
+
+    direction = (detector_center - vertex).unit();
 }
 
 void PrimaryGeneratorAction::GenerateSquare(G4double number) {
@@ -46,11 +53,20 @@ void PrimaryGeneratorAction::GenerateSquare(G4double number) {
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
     G4ThreeVector vertex;
-    GenerateVertex(vertex);
+    // Retrieve the event ID
+    G4int eventID = anEvent->GetEventID();
+    // Map the event ID to an angle in degrees
+    G4int angleDegrees = (eventID*10) % 360; // Wrap around after 360
+    // Convert the angle to radians
+    G4double angleRadians = angleDegrees * (M_PI / 180.0);
+
+    //GenerateVertex(6.*cm, 2.*cm, CLHEP::pi/2., vertex);
+    GenerateVertex(6.*cm, 3.*cm, angleRadians, vertex);
+    
     particleGun->SetParticlePosition(vertex);
 
     G4ThreeVector direction;
-    GenerateDirection(direction);
+    GenerateDirection(4.*cm, 0., 0., vertex, direction);
     particleGun->SetParticleMomentumDirection(direction);
     
     // Output for debug
